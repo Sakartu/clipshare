1faffbefaf79ca352cf746cf6f8eb6b5b6fa3a52
Peter Wagenaar <sakartu@gmail.com>
2011-06-14 20:32:40 +0200

    announcer finished

--- announcer/clipshare_announcer.py
+++ announcer/clipshare_announcer.py
@@ -1,5 +1,6 @@
 from util.infinite_timer import InfiniteTimer
 import util.clipshare_util as util
+import util.constants as constants
 import logging
 import sys
 
@@ -16,17 +17,30 @@ class ClipshareAnnouncer():
 			except ValueError:
 				pass
 				#weird value in conf, take default
-		#get the local ip
-		ip = util.getip(self.conf)
+		#get the local ip and set it for later use
+		ip = util.get_ip(self.conf)
+		self.conf['ip'] = ip
+		port = constants.PORT
+		if 'port' in self.conf:
+			try:
+				port = int(self.conf['port'])
+			except ValueError:
+				#weird value in conf, take default
+				pass
+
 		if ip:
-			logger.info('Found local ip %s!' % (ip))
-			t = InfiniteTimer(interval, announce, [ip, port])
+			self.logger.info('Found local ip %s!' % (ip))
+			self.logger.info('Setting up announcer with interval %d...' % (interval))
+			t = InfiniteTimer(interval, self.announce, [ip, port], immediate=True)
+			t.daemon = True
+			t.start()
 		else:
-			logger.error('No local ip-address could be found and none was specified, exitting!')
+			self.logger.error('No local ip-address could be found and none was specified, exitting!')
 			sys.exit(2)
 
 	def announce(self, ip, port):
-		msg = "CSHELO:%s:%d:OLEHSC" % (ip, port)
+		self.logger.debug('Announcing %s:%d!' % (ip, port))
+		msg = util.encrypt(self.conf['keyfile'],"CSHELO:%s:%d:OLEHSC" % (ip, port))
 		util.broadcast(msg, constants.PORT)
 
 
