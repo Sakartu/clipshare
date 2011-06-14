5c399d881dc5b9a5320ec6d2e0a6b0392b2639fe
Peter Wagenaar <sakartu@gmail.com>
2011-06-14 17:06:52 +0200

    finished server

--- server/clipshare_server.py
+++ server/clipshare_server.py
@@ -1,17 +1,19 @@
+from ncrypt.cipher import CipherError
 import socket
 import traceback
 import logging
 import util.clipshare_util as util
 import client.clipshare_remote_client as remote
-import threading.Thread
+from threading import Thread
 
-class ClipshareRegistrationServer(threading.Thread):
+class ClipshareServer(Thread):
 	s = None
 	logger = logging.getLogger('ClipshareRegistrationServer')
 
 	clientlist = []
 
 	def __init__(self, port, buf_size, conf):
+		Thread.__init__(self)
 		self.port = port
 		self.buf_size = buf_size
 		self.conf = conf
@@ -22,21 +24,32 @@ class ClipshareRegistrationServer(threading.Thread):
 
 	def run(self):
 		'''
-		This method starts the Clipshare registration server and starts
+		This method starts the Clipshare server and starts
 		listening on the given port.
-
-		setup() should have been called before this method is called
 		'''
 		while True:
-			message, address = self.s.recvfrom(buf_size)
-			if self.conf != None and self.conf.getboolean('encryption', 'enabled'):
-				decrypted = util.decrypt(self.conf.get('encryption', 'keyfile'))
-			else:
-				decrypted = message
+			message, address = self.s.recvfrom(self.buf_size)
+			self.logger.info('Got message, decrypting')
+			decrypted = None
+			try:
+				decrypted = util.decrypt(self.conf['keyfile'], message)
+			except CipherError:
+				#something went wrong during decrypting, probably garbage
+				self.logger.info('Got some garbage after decrypting, ignoring')
+				
 			t = util.get_message_type(decrypted)
 			if t == 'CSHELO':
 				#new client found, add it to the list
 				(ip, port) = util.parse_cs_helo_msg(decrypted)
 				self.clientlist.append(remote.ClipshareRemoteClient(ip, port))
+				self.logger.info('Added client with ip %s on port %d!' % (ip, port))
+			elif t == 'CSCONTENT':
+				#new content, put it in clipboard
+				content = util.parse_cs_content_msg(decrypted)
+				self.logger.info('Got new content: "' + content + '"')
+				util.store_in_clipboard(content)
+			else:
+				#garbage
+				pass
 
 
