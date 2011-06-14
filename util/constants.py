479961f7182397c7f0a332ab933a1b126760f8fa
Peter Wagenaar <sakartu@gmail.com>
2011-06-14 18:16:16 +0200

    added some constants values

--- util/constants.py
+++ util/constants.py
@@ -1,2 +1,4 @@
-PORT=50244
-BUF_SIZE=8192
+PORT=50244		#default port to listen on
+BUF_SIZE=8192	#in bytes
+INTERVAL=5		#number of seconds between clipboard checks
+ANNOUNCE=20		#number of seconds between CSHELO messages
